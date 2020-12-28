374206d158409297f345595bcc5df47e78c0d182
Milen <milenppavlov@gmail.com>
2020-12-28 17:39:16 +0000

    Client side changes module 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -18,8 +18,6 @@ namespace API
             _config = config;
         }
 
-        //public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
@@ -34,6 +32,8 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +49,10 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors(policy =>
+            {
+                policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+            });
 
             app.UseAuthorization();
 
