8d9e5ad81194f1da5271b0cc30b3b0ead4064bf8
Milen <milenppavlov@gmail.com>
2020-12-30 18:27:13 +0000

    minor change

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
   { path: 'not-found', component: NotFoundComponent },
   { path: 'server-error', component: ServerErrorComponent },
   { path: 'errors', component: TestErrorsComponent },
-  { path: '**', component: HomeComponent, pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
